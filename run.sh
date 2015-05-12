b1275501fdc40a3f3ecb0b88506477cf00615e76
Ramon Brooker <rbrooker@aetherealmind.com>
2015-05-12 16:25:06 -0400

    added support to load plugins on run

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ PATH_LOGS=/es/logs
 PATH_WORK=/work
 PATH_PLUGINS=/es/plugins
 
+PLUGINS_TO_LOAD=
 
 
 # [TODO] get this working 
@@ -20,6 +21,11 @@ PATH_PLUGINS=/es/plugins
 #  PATH_PLUGINS=/es/plugins
 #  esac
 #done
+
+if [[ -z PLUGINS_TO_LOAD ]] then
+  /es/bin/plugin -i $PLUGINS_TO_LOAD
+fi
+
 
 /es/bin/elasticsearch  --path.plugins=${PATH_PLUGINS}  --path.logs=${PATH_LOGS} --path.work=${PATH_WORK} --path.data=${PATH_DATA}  --node.name=${NODE_NAME}
 
